0c919772ef6746691809e51cfbb2a71218e75709
Scot Walters <scowalt@gmail.com>
2023-01-08 15:19:22 -0800

    Implemented `Remove-InvalidRepos`

--- src/helpers.psm1
+++ src/helpers.psm1
@@ -1,2 +1,21 @@
+$ValidRepos = @(
+    "BizHawk-Vanguard",
+    "dolphin-vanguard",
+    "FileStubTemplate-Cemu",
+    "FileStub-Vanguard",
+    "melonDS-Vanguard"
+    "pcsx2-Vanguard",
+    "ProcessStub-Vanguard",
+    "RTCV",
+    "xemu-Vanguard"
+)
+
 function Remove-InvalidRepos([System.Collections.ArrayList]$repos) {
+    for ($i = 0; $i -lt $repos.Count; $i++) {
+        $repo = $repos[$i]
+        if (-not $ValidRepos.Contains($repo)) {
+            Write-Host "Invalid repo: $repo" -ForegroundColor Red
+            $repos.Remove($repo)
+        }
+    }
 }
