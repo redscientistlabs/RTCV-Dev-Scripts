9cc897e178a35f7e4d2891606f3ce021288db53b
Scot Walters <scowalt@gmail.com>
2023-01-19 14:00:44 -0800

    Added "force" switch

--- setup.ps1
+++ setup.ps1
@@ -31,6 +31,8 @@ param (
     # Clone with HTTPS instead of SSH
     [switch]$https = $false,
 
+    [switch]$force = $false,
+
     # TODO - Building on clone may be nice for some devs.
     # [switch]$build = $false,
 
@@ -167,7 +169,7 @@ function Clone-Repo([string]$repo, [string]$directory, [bool]$silent) {
     $repoUrl = $repoBaseUrl + $repo + '.git';
     $repoDirectory = $directory + '\' + $repo;
     $branch = ($ValidRepos | Where-Object -FilterScript { $_.Name -eq $repo }).Branch
-    if (Test-Path $repoDirectory) {
+    if ((-not $force) -and (Test-Path $repoDirectory)) {
         Write-Host "Repo '$repo' already exists locally." -ForegroundColor Yellow
         if ($silent) {
             Write-Host "Skipping..." -ForegroundColor Yellow
